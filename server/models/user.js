e52c5edd4e5d08ffb2561de43cb45b57ebfc41c6
ajayruhela <jrhl0013@gmail.com>
2018-10-05 00:46:52 +0200

    mongoose queries added

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,11 @@
 var mongoose = require ('mongoose');
 var User = mongoose.model('User', {
+    name:{
+        type:String,
+        required: true, // to avoid adding empty docs with __v and _id only
+        minlength:4,
+        trim:true
+    },
     email:{
         type:String,
         required: true, // to avoid adding empty docs with __v and _id only
