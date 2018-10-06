a9dfd13058d441cb24d20a34a088a3ed63547af9
ajayruhela <jrhl0013@gmail.com>
2018-10-07 00:49:04 +0200

    test cases added for todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,19 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const ObjectID = require('mongodb').ObjectID;
 const {app} = require('../server');
 const{Todo} = require('../models/todo');
 var countTodos =0;
 
-const todos = [{text:'First test todo'},{text:'Second test todo'}];
+const todos = [
+    {
+        _id : new ObjectID(),
+        text:'First test todo'
+    },
+    {
+        _id : new ObjectID(),
+        text:'Second test todo'
+    }];
 beforeEach((done)=>{
   Todo.deleteMany({}).then(()=>{
       return Todo.insertMany(todos)
@@ -68,3 +76,38 @@ it('should not create todo with invalid body data', (done) => {
     
       });
     });
+// Get todos/id
+    describe('GET /todos/id',()=>{
+
+        it('should get 404 due to invalid id',(done)=>{
+          
+          request(app)
+          .get('/todos/5bb68ebe9c961127cc6509b')
+          .expect(404)
+        //   .expect((res)=>{
+        //       expect(res.body).toBe('Not a valid Id');
+        //   })
+          .end(done);
+        
+          });
+        
+        
+        it('should get 404 as no to do with this id',(done)=>{
+          
+            request(app)
+            .get('/todos/5bb68ebe9c961127cc6509b6')
+            .expect(404)
+            .end(done);
+          });
+
+        it('should get 200 and the todo item with the given id',(done)=>{
+          
+                request(app)
+                .get(`/todos/${todos[0]._id.toHexString()}`)
+                .expect(200)
+                .expect((res)=>{
+                    expect(res.body.todo.text).toBe(todos[0].text)
+                })
+                .end(done);
+              });
+        });
