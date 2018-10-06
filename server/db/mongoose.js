6897b5909e9e9cb3b8407047e847941354c27cca
ajayruhela <jrhl0013@gmail.com>
2018-10-07 01:19:54 +0200

    comlete config to deploy on Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose= require('mongoose');
 var uri = require('../server.config').todoApp_dbUri;
 mongoose.Promise = global.Promise;
-mongoose.connect(uri ,{useNewUrlParser :true},(err, client) =>{
+mongoose.connect(process.env.MONGODB_URI ||  uri ,{useNewUrlParser :true},(err, client) =>{
     if (err) {
         console.log(err);
     }else{
